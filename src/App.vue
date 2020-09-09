60dcdcd3ce1685e8ecb61a380005a39c41c0d2f0
lixu <2063695593@qq.com>
2020-09-09 12:39:37 +0800

    fist

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,27 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <button @click="loadData()">加载API服务器上的数据</button>
+    <div>{{userList}}</div>
   </div>
 </template>
-
+<script>
+import axios from 'axios'
+export default {
+  data: function () {
+    return {
+      userList: [],
+    };
+  },
+  methods:{
+    loadData(){
+      let url='http://codeorder.applinzi.com/user';
+      axios.get(url).then((res)=>{
+        this.userList=res.data;
+      });
+    }
+  }
+};
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -15,18 +29,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
